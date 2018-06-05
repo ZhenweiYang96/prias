a7511efedf94f1ce8f8871b7b0c25ff8b41c405c
Ani <anirudhtomer@gmail.com>
2018-06-05 15:45:53 +0200

    Update hazardplot.R

--- src/decision_analytic/simulationStudy/hazardplot.R
+++ src/decision_analytic/simulationStudy/hazardplot.R
@@ -34,6 +34,33 @@ getSplineLogBaselineHazard = function(jointModelData, timePoint){
           ord = jointModelData$mvJoint_psa_simDs$control$ordSpline, outer.ok = T) %*% jointModelData$mvJoint_psa_simDs$statistics$postwMeans$Bs_gammas
 }
 
+semiParamBH = function (coxObject, knots = NULL, length.knots = 6) {
+  Time <- coxObject$y[, 1]
+  d <- coxObject$y[, 2]
+  n <- length(Time)
+  if (is.null(knots)) {
+    Q <- length.knots + 1
+    knots <- unique(quantile(Time, seq(0, 1, len = Q + 1), 
+                             names = FALSE)[-c(1, Q + 1)])
+    knots <- knots + 1e-06
+    if (max(knots) > max(Time)) 
+      knots[which.max(knots)] <- max(Time) - 1e-06
+  }
+  knots <- c(0, sort(knots), max(Time) + 1)
+  Q <- length(knots) - 1
+  ind <- findInterval(Time, knots, rightmost.closed = TRUE)
+  D <- matrix(0, n, Q)
+  D[cbind(seq_along(ind), ind)] <- 1
+  D <- c(D * d)
+  Tiq <- outer(Time, knots, pmin)
+  T <- c(Tiq[, 2:(Q + 1)] - Tiq[, 1:Q])
+  X <- coxObject$x[rep(seq_len(n), Q), ]
+  ND <- suppressWarnings(data.frame(Time = T, D = D, X, xi = gl(Q, 
+                                                                n), check.names = FALSE)[T > 0, ])
+  return(list(glm(D ~ . + offset(log(Time)) - Time - 1, family = poisson, 
+      data = ND), knots))
+}
+
 getSemiParametricLogBaselineHazard = function(jointModelData, semiParamCoeffs, knots, timePoint){
   semiParamHazard = semiParamCoeffs[max(which(knots<=timePoint))]
 }
@@ -48,9 +75,8 @@ for(i in 1:length(savedFiles)){
   load(savedFiles[i])
   print(paste("Reading the file number:", i))
   
-  semiParamCoeffs = c(semiParamBH(jointModelData$survModel_simDs, length.knots = 20)[[1]]$coefficients[-c(1,2)], 0)
-  knots = semiParamBH(jointModelData$survModel_simDs, length.knots = 20)[[2]]
-  
+  #semiParamCoeffs = c(semiParamBH(jointModelData$survModel_simDs, length.knots = 20)[[1]]$coefficients[-c(1,2)], 0)
+  #knots = semiParamBH(jointModelData$survModel_simDs, length.knots = 20)[[2]]  
   #logHazardMatrix[i, ] = sapply(times, getSemiParametricLogBaselineHazard, jointModelData=jointModelData, semiParamCoeffs=semiParamCoeffs, knots=knots)
   logHazardMatrix[i, ] = sapply(times, getSplineLogBaselineHazard, jointModelData=jointModelData)
     
